eeaed750bfe1f68937ff614064613e0d97c887e9
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2025-06-05 12:03:36 +0100

    project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,38 +1,84 @@
 import React from 'react'
+import data from '../data/data'
+import { CiShare1 } from "react-icons/ci";
 
 const Project = () => {
   return (
-    <div>
-        <h1></h1>
-        <div>
-            <div>
-                <div>
-                    <img/>
+    <div className='black-cnt'>
+        <div className='project'>
+        <h1><span>{data.eng.my}</span> {data.eng.project}</h1>
+        <div className='projects-cnt'>
+            <div className='projects-itm'>
+                <div className='img-cnt'>
+                    <img src='p1.svg'/>
                 </div>
-                <div>
-                    <h1></h1>
-                    <h2></h2>
-                    <p></p>
-                    <a>
+                <div className='txt'>
+                    <h1>01</h1>
+                    <h2>NFTs store</h2>
+                    <p>
+                        i use react and django drf and three.js stripe
 
+                    </p>
+                    <a href=''>
+                        <CiShare1/>
                     </a>
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <h1></h1>
-                    <h2></h2>
-                    <p></p>
-                    <a>
-                        
+            <div className='projects-itm'>
+                
+                <div className='txt'>
+                    <h1>01</h1>
+                    <h2>NFTs store</h2>
+                    <p>
+                        i use react and django drf and three.js stripe
+
+                    </p>
+                    <a href=''>
+                        <CiShare1/>
                     </a>
                 </div>
-                <div>
-                    <img/>
+                <div className='img-cnt'>
+                    <img src='p2.svg'/>
+                </div>
+            </div>
+
+            <div className='projects-itm'>
+                <div className='img-cnt'>
+                    <img src='p3.svg'/>
+                </div>
+                <div className='txt'>
+                    <h1>01</h1>
+                    <h2>NFTs store</h2>
+                    <p>
+                        i use react and django drf and three.js stripe
+
+                    </p>
+                    <a href=''>
+                        <CiShare1/>
+                    </a>
+                </div>
+            </div>
+
+            <div className='projects-itm'>
+                
+                <div className='txt'>
+                    <h1>01</h1>
+                    <h2>NFTs store</h2>
+                    <p>
+                        i use react and django drf and three.js stripe
+
+                    </p>
+                    <a href=''>
+                        <CiShare1/>
+                    </a>
+                </div>
+                <div className='img-cnt'>
+                    <img src='p4.svg'/>
                 </div>
             </div>
         </div>
+    </div>
     </div>
   )
 }
